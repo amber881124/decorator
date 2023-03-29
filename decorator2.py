65b238d91f1916d08251d41db56c84345ccda0ad
momo <zyongzhu24@gmail.com>
2023-03-29 14:33:08 +0800

    decorator time - vesion3 (2@)

--- decorator2.py
+++ decorator2.py
@@ -1,14 +1,21 @@
 import time
 
+def print_name(func):
+    def wrapper():
+        print(f'現在正在運行 : {func.__name__}')
+        func()
+    return wrapper
+
 def time_counter(func):
     def wrapper():
         start = time.time()
         func()
         end = time.time()
-        print(f'{func.__name__}運行共花費 : {end - start}秒')
+        print(f'運行共花費 : {end - start}秒')
     return wrapper
 
 @ time_counter
+@ print_name
 def loop():
     for i in range(1000000):
         i += 1
